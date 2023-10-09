c6979daa61e64ff77e97a93d9dfdd9521f2a7ce9
Victor Scarsi <victorscarsi@gmail.com>
2023-10-09 19:40:54 -0300

    Projeto finalizado com as 8 consultas.

--- Main.java
+++ Main.java
@@ -15,9 +15,9 @@ public class Main {
         jogadorComMaisGols(gols);
         jogadorComMaisGolsDePenalty(gols);
         jogadorComMaisGolsContra(gols);
-        jogadorComMaisAmarelos();
-        jogadorComMaisVermelhos();
-        placarDaPartidaComMaisGols();
+        jogadorComMaisAmarelos(cartoes);
+        jogadorComMaisVermelhos(cartoes);
+        placarDaPartidaComMaisGols(full);
     }
 
     public static void maiorVencedor2008(CSVReader arquivo) {
@@ -249,9 +249,141 @@ public class Main {
         }
     }
 
-    public static void jogadorComMaisAmarelos() { }
+    public static void jogadorComMaisAmarelos(CSVReader arquivo) {
+        // Arquivo: cartoes
+        // "partida_id","rodata","clube","cartao","atleta","num_camisa","posicao","minuto"
+        //  0          , 1      , 2     , 3      , 4      , 5          , 6       , 7
 
-    public static void jogadorComMaisVermelhos() { }
+        // Criando a lista de registros de jogadores que mais receberam cartões amarelos para comparação.
+        ArrayList<String> registros = new ArrayList<String>();
+        for (int x = 1; x < arquivo.linhas.size(); x++) {
+            try {
+                if ((arquivo.linhas.get(x)[3] != null) && (arquivo.linhas.get(x)[4] != null) &&
+                        (arquivo.linhas.get(x)[3].equals("Amarelo"))) {
+                    registros.add(arquivo.linhas.get(x)[4]);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-    public static void placarDaPartidaComMaisGols() { }
+        // Criando a lista de jogadores e contando quantas vezes o nome deles aparece na lista de registros de jogadores mais receberam cartões amarelos.
+        List<String> jogadores = registros.stream().distinct().toList();
+        int cont = 0;
+        int maiorQtdeCartoes = 0;
+        ArrayList<String[]> amarelados = new ArrayList<String[]>(); // [jogador][qtde cartoes]
+        for (int x = 0; x < jogadores.size(); x++) {
+            cont = 0;
+            for (int y = 0; y < registros.size(); y++) {
+                if (registros.get(y).equals(jogadores.get(x))) {
+                    cont++;
+                }
+            }
+            if (cont > maiorQtdeCartoes) {
+                maiorQtdeCartoes = cont;
+            }
+            amarelados.add(new String[]{jogadores.get(x), String.valueOf(cont)});
+            //System.out.println(jogadores.get(x) + " - " + cont);
+        }
+
+        // Imprimindo o resultado
+        System.out.println("------------------------------");
+        System.out.println("Jogador(es) que mais recebeu cartão amarelo: ");
+        for (String[] aux : amarelados) {
+            if (Integer.parseInt(aux[1]) == maiorQtdeCartoes) {
+                System.out.println(aux[0] + " - " + aux[1] + " cartões amarelos");
+            }
+        }
+    }
+
+    public static void jogadorComMaisVermelhos(CSVReader arquivo) {
+        // Arquivo: cartoes
+        // "partida_id","rodata","clube","cartao","atleta","num_camisa","posicao","minuto"
+        //  0          , 1      , 2     , 3      , 4      , 5          , 6       , 7
+
+        // Criando a lista de registros de jogadores que mais receberam cartões vermelhos para comparação.
+        ArrayList<String> registros = new ArrayList<String>();
+        for (int x = 1; x < arquivo.linhas.size(); x++) {
+            try {
+                if ((arquivo.linhas.get(x)[3] != null) && (arquivo.linhas.get(x)[4] != null) &&
+                        (arquivo.linhas.get(x)[3].equals("Vermelho"))) {
+                    registros.add(arquivo.linhas.get(x)[4]);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Criando a lista de jogadores e contando quantas vezes o nome deles aparece na lista de registros de jogadores que  mais receberam cartões vermelhos.
+        List<String> jogadores = registros.stream().distinct().toList();
+        int cont = 0;
+        int maiorQtdeCartoes = 0;
+        ArrayList<String[]> expulsos = new ArrayList<String[]>(); // [jogador][qtde cartoes]
+        for (int x = 0; x < jogadores.size(); x++) {
+            cont = 0;
+            for (int y = 0; y < registros.size(); y++) {
+                if (registros.get(y).equals(jogadores.get(x))) {
+                    cont++;
+                }
+            }
+            if (cont > maiorQtdeCartoes) {
+                maiorQtdeCartoes = cont;
+            }
+            expulsos.add(new String[]{jogadores.get(x), String.valueOf(cont)});
+            //System.out.println(jogadores.get(x) + " - " + cont);
+        }
+
+        // Imprimindo o resultado
+        System.out.println("------------------------------");
+        System.out.println("Jogador(es) que mais recebeu cartão vermelho: ");
+        for (String[] aux : expulsos) {
+            if (Integer.parseInt(aux[1]) == maiorQtdeCartoes) {
+                System.out.println(aux[0] + " - " + aux[1] + " cartões vermelhos");
+            }
+        }
+    }
+
+    public static void placarDaPartidaComMaisGols(CSVReader arquivo) {
+        //Arquivo: full
+        // "ID","rodata","data","hora","mandante","visitante","formacao_mandante","formacao_visitante","tecnico_mandante","tecnico_visitante","vencedor","arena","mandante_Placar","visitante_Placar","mandante_Estado","visitante_Estado"
+        //  0  , 1      , 2    , 3    , 4        , 5         , 6                 , 7                  , 8                , 9                 , 10       , 11    , 12              , 13               , 14              , 15
+
+        int cont, maior = 0;
+        int golsCasa, golsVisitante = 0;
+        // Verifica a maior quantidade de gols
+        for (int x = 1; x < arquivo.linhas.size(); x++) {
+            try {
+                golsCasa = Integer.parseInt(arquivo.linhas.get(x)[12]);
+                golsVisitante = Integer.parseInt(arquivo.linhas.get(x)[13]);
+                int soma = golsCasa + golsVisitante;
+                if (soma > maior) {
+                    maior = soma;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Encontrando os registros de jogos com mais gols.
+        ArrayList<String[]> melhoresJogos = new ArrayList<String[]>(); // [mandante][visitante][mandante-placar][visitante-placar]
+        for (int x = 1; x < arquivo.linhas.size(); x++) {
+            golsCasa = Integer.parseInt(arquivo.linhas.get(x)[12]);
+            golsVisitante = Integer.parseInt(arquivo.linhas.get(x)[13]);
+            if ((golsCasa + golsVisitante) == maior) {
+                melhoresJogos.add(new String[]{
+                   arquivo.linhas.get(x)[4],
+                        arquivo.linhas.get(x)[5],
+                        arquivo.linhas.get(x)[12],
+                        arquivo.linhas.get(x)[13]
+                });
+            }
+        }
+
+        // Imprimindo o resultado
+        System.out.println("------------------------------");
+        System.out.println("Placar da(s) partida(s) com mais gols: ");
+        for (String[] aux : melhoresJogos) {
+            System.out.println(aux[0] + " " + aux[2] + " x " + aux[3] + " " + aux[1]);
+        }
+    }
 }
